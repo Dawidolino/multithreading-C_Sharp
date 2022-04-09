2a7bb5a0dd54012ef804a078ba77684947c25061
Dawid Ruth <73530948+Dawidolino@users.noreply.github.com>
2022-04-09 16:41:04 +0200

    Add files via upload

---
+++ multithreading/multithreading/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace multithreading_Dawid_Ruth_58411
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 30);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "thread 1\r\n(green)\r\n";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(17, 48);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(65, 30);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "thread 2\r\n(orange)\r\n";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(17, 84);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(65, 30);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "thread 3\r\n(purple)\r\n";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(17, 120);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(102, 30);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "thread 4 and 5\r\n(blue and black)\r\n";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 286);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Multithreading";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ multithreading/multithreading/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace multithreading_Dawid_Ruth_58411
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rd = new Random();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = rd.Next(0, this.Width);
+                    int height = rd.Next(0, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.ForestGreen, 1), new Rectangle(width, height, 10, 10));
+                    Thread.Sleep(100); 
+                }          
+            })
+            { IsBackground = true };
+            thread1.Start();
+
+        }
+        Random rd;
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread thread2 = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = rd.Next(0, this.Width);
+                    int height = rd.Next(0, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Orange, 1), new Rectangle(width, height, 10, 10));
+                    Thread.Sleep(100);
+                }
+            })
+            { IsBackground = true };
+            thread2.Start();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread thread3 = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = rd.Next(0, this.Width);
+                    int height = rd.Next(0, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Purple, 1), new Rectangle(width, height, 10, 10));
+                    Thread.Sleep(100);
+                }
+            })
+            { IsBackground = true };
+            thread3.Start();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            Thread thread4 = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = rd.Next(0, this.Width);
+                    int height = rd.Next(0, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Black, 1), new Rectangle(width, height, 10, 10));
+                    Thread.Sleep(100);
+                }
+            })
+            { IsBackground = true };
+            thread4.Start();
+
+            Thread thread5 = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = rd.Next(0, this.Width);
+                    int height = rd.Next(0, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Blue, 1), new Rectangle(width, height, 10, 10));
+                    Thread.Sleep(100);
+                }
+            })
+            { IsBackground = true };
+            thread5.Start();
+        }
+    }
+
+}
